ca271696870fe3018e797467c120c7de9d33904d
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2019-10-18 11:56:26 -0400

    created get request for bubblespage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import PrivateRoute from './components/PrivateRoute'
 
 import Login from "./components/Login";
@@ -12,6 +12,19 @@ function App() {
       <div className="App">
         <Route exact path="/" component={Login} />
         <PrivateRoute path='/bubbles' component={BubblePage} />
+        <ul style={{listStyleType:'none', textAlign:'left', marginRight:'10px'}}>
+          <li>
+            <Link to='/' style={{textDecoration:'none', color:'black', }}>
+            Login
+            </Link>
+          </li>
+          <br />
+          <li>
+            <Link to='/bubbles' style={{textDecoration:'none', color:'black'}}>
+            Bubble Page
+            </Link>
+          </li>
+        </ul>
       </div>
     </Router>
   );
